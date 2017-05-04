ab9256e3a47370bc068a90204df4643fb1df0606
Ilya Isaev <me@ilyaisaev.com>
2017-05-04 16:03:15 +0700

    Test ready version

--- run.sh
+++ run.sh
@@ -6,20 +6,21 @@ Template() {
     [[ -z "$2" ]] &&  sed -i "s/{{ $1 }}/$3/g" $4 || sed -i "s/{{ $1 }}/$2/g" $4
 }
 
+CONF="/etc/aerospike/aerospike.conf"
+
 Template data_file_size_gb "$AERO_DATA_FILE_SIZE_GB" 1 $CONF
 Template data_eviction_days "$AERO_DATA_EVICTION_DAYS" 1 $CONF
 Template data_memory_size_gb "$AERO_DATA_MEMORY_SIZE_GB" 2 $CONF
 
-if [ "$1" = 'asd' ]; then
-	set -- "$@" --foreground
-fi
-
-if [ -n "$SEEDER" ]; then
-	exec "$@"
+if [ -n "$AERO_MASTER" ]; then
+	Assign() {
+		dockerize -wait tcp://127.0.0.1:3000
+		dockerize -wait tcp://$AERO_NODE1:3002
+		dockerize -wait tcp://$AERO_NODE2:3002
+		asinfo -v 'tip:host=$AERO_NODE1;port=3002'
+		asinfo -v 'tip:host=$AERO_NODE2;port=3002'
+	}
+	 Assign & asd --foreground
 else
-	#maybe it's working :D
-	asinfo -v 'tip:host=<ADDR>;port=3002'
-	#I need dockerize here
-	#wait on port
-	exec "$@"
+	asd --foreground
 fi
